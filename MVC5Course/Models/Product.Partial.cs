43c497f47168d7e7520989e32c9f728f9fa64cb5
Julian-Chu <yulang.chu@gmail.com>
2017-03-12 13:29:20 +0800

    11 練習擴充實體資料模型 ( 使用 EFGeneratePartialClass.tt 程式碼產生器範本 )

---
+++ MVC5Course/Models/Product.Partial.cs
@@ -0,0 +1,33 @@
+namespace MVC5Course.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+    
+    public partial class ProductMetaData
+    {
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [Required(ErrorMessage = "請輸入商品名稱({0})")]
+        [DisplayName("商品名稱")]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(10, 9999999999, ErrorMessage = "({0}) 金額錯誤")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public Nullable<decimal> Price { get; set; }
+        [Required]
+        public Nullable<bool> Active { get; set; }
+        [Required]
+        public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
