25b62e8645894657fbbd7644c6d4e86dfb5828b2
Julian-Chu <yulang.chu@gmail.com>
2017-03-12 16:05:16 +0800

    14 修改 ProductRepository 將 Stock >= 500 的商品全部隱藏 - 在Repository內藉由override修改method滿足需求

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -10,6 +10,17 @@ namespace MVC5Course.Models
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
         }
+
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => p.Stock < 500);
+        }
+
+        public IQueryable<Product> All(bool showAll)
+        {
+            if (showAll) return base.All();
+            else return this.All();
+        }
     }
 
     public  interface IProductRepository : IRepository<Product>
