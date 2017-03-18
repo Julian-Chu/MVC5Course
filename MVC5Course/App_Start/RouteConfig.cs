8c8ec85659a88c73ba05707017772e083c22f46e
Julian-Chu <yulang.chu@gmail.com>
2017-03-18 11:40:51 +0800

    19 修正預設路由，將網址結構變成 /Home.Index 這種網址格式

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -14,11 +14,11 @@ namespace MVC5Course
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             ///For 19 修正預設路由，將網址結構變成 /Home.Index 這種網址格式
-            //routes.MapRoute(
-            //    name: "NewRoute",
-            //    url: "{controller}.{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "NewRoute",
+                url: "{controller}.{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
                 name: "Default",
