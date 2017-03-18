1de69b6df629b2f8ce9416cef18f29c54164174d
Julian-Chu <yulang.chu@gmail.com>
2017-03-18 14:08:29 +0800

    示範 PartialView 的寫法

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -9,9 +9,26 @@ namespace MVC5Course.Controllers
     public class ARController : Controller
     {
         // GET: AR
+        public ActionResult Index1()  
+        {
+            string data = "123";
+            return View(data); //會掛掉
+        }
+
         public ActionResult Index()
         {
-            return View();
+            string data = "123";
+            return View((object)data);
+        }
+
+        public ActionResult View2()
+        {
+            return PartialView("Index");  //由controller決定沒有Layout : return PartialView("Index");
+        }
+
+        public ActionResult View3()
+        {
+            return View();          //由View決定沒有Layout  :  Layout = null in View3.cshml;
         }
     }
 }
